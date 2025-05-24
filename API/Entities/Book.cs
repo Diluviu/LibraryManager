865d41e8d873b418bea4dc7ef8d2b5180f545dc0
Diluviu <86706425+Diluviu@users.noreply.github.com>
2025-05-24 17:56:35 +0300

    Initial commit

---
+++ API/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public bool IsAvailable { get; set; }
+    
+    
+}
